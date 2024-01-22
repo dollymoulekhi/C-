d6d879182c3e47039c04ce60089cde69fd313ffd
Dolly Moulekhi <dollymoulekhi97@gmail.com>
2024-01-22 11:24:31 -0500

    while loop addition

--- loops.cpp
+++ loops.cpp
@@ -2,10 +2,26 @@
 
 using namespace std;
 
-int main() 
+
+// for loop
+
+// int main() 
+// {
+//     for (int i=0; i<10; i++) {
+//         cout << "Helllo " << i <<endl;
+//     }
+
+// }
+
+//  while loop
+
+int main()
 {
-    for (int i=0; i<10; i++) {
-        cout << "Helllo " << i <<endl;
+    int i=0;
+    while (i<10) {
+        cout << "Hello " << i << endl;
+        i++;
+
     }
 
 }
