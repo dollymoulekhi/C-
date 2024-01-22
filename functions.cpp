5cc1e416890d102690f5e54274bfd139661488e6
Dolly Moulekhi <dollymoulekhi97@gmail.com>
2024-01-21 21:17:47 -0500

    new changes in function

--- functions.cpp
+++ functions.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
-void say_hello()
+void say_hello( std:: string user_name, int user_age)
 {
-    std:: cout << "Hello! Enjoying learning C++" << std::endl;
-    std:: cout << "Testing the function" << std::endl;
+    std:: cout << "Hello " << user_name << " your age is " << user_age << std::endl;
 }
+
+std:: string user_name;
+int user_age;
 
 int main()
 
+
 {
-    say_hello();
+
+    std:: cout << "Please enter your name ";
+    std:: cin >> user_name;
+
+    std:: cout << "Please enter your age ";
+    std:: cin >> user_age;
+    say_hello(user_name, user_age);
     return 0;
 }
