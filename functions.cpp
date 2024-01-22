c917ef793b00d69195bef7c3532ab9b383cc0dd7
Dolly Moulekhi <dollymoulekhi97@gmail.com>
2024-01-21 20:25:41 -0500

    learning function creation

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void say_hello()
+{
+    std:: cout << "Hello! Enjoying learning C++" << std::endl;
+    std:: cout << "Testing the function" << std::endl;
+}
+
+int main()
+
+{
+    say_hello();
+    return 0;
+}
